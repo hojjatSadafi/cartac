fc3c5e2fbcab1f6ed43386ea7dbe1e8eef2e3cf9
hojjat sadafi <161528589+hojjatSadafi@users.noreply.github.com>
2025-01-06 00:36:31 +0330

    complete car detail sub images slider

--- public/js/car-detail.js
+++ public/js/car-detail.js
@@ -1,8 +1,22 @@
 const healthDropDowns = document.querySelectorAll('.healthDropDown')
+const mainCarImg = document.getElementById('mainCarImg')
+const subImageWrapper = document.querySelector('.subImage-wrapper')
 
+
+// funcs
+
+
+// events
 healthDropDowns.forEach(healthDropDown => {
     healthDropDown.addEventListener('click',()=>{
         healthDropDown.classList.toggle('healthDropDown--active')
         healthDropDown.children[1].classList.toggle('closed')
     })
 });
+
+subImageWrapper.addEventListener('click',event=>{
+    if(event.target.tagName.toLowerCase()==='img'){
+      mainCarImg.setAttribute('src',event.target.src)
+      scrollTo(0,0)
+    }
+})
