70d34ed85b317596dcccfd3e1d549dbe6962e8a3
hojjat sadafi <161528589+hojjatSadafi@users.noreply.github.com>
2025-01-06 22:14:11 +0330

    complte modal

--- public/js/car-detail.js
+++ public/js/car-detail.js
@@ -1,10 +1,28 @@
 const healthDropDowns = document.querySelectorAll('.healthDropDown')
 const mainCarImg = document.getElementById('mainCarImg')
 const subImageWrapper = document.querySelector('.subImage-wrapper')
+const modalCloseBtn = document.getElementById('modal-close-btn')
+const carImagesModal = document.querySelector('.car-images-modal')
+const overlay = document.querySelector('.overlay')
+const modalTitles = document.querySelectorAll('.modal-title')
 
 
 // funcs
-
+function showModal(){
+    carImagesModal.classList.remove('hide')
+    overlay.classList.remove('hide')
+    document.body.classList.add('overflow-hidden')
+}
+function closeModal(){
+    carImagesModal.classList.add('hide')
+    overlay.classList.add('hide')
+    document.body.classList.remove('overflow-hidden')
+}
+function hideAllModalTitleActive(){
+    modalTitles.forEach(modalTitle=>{
+        modalTitle.classList.remove('modal-title--active')
+    })
+}
 
 // events
 healthDropDowns.forEach(healthDropDown => {
@@ -19,4 +37,14 @@ subImageWrapper.addEventListener('click',event=>{
       mainCarImg.setAttribute('src',event.target.src)
       scrollTo(0,0)
     }
+})
+
+modalCloseBtn.addEventListener('click',closeModal)
+overlay.addEventListener('click',closeModal)
+mainCarImg.addEventListener('click',showModal)
+modalTitles.forEach(modalTitle=>{
+    modalTitle.addEventListener('click',event=>{
+        hideAllModalTitleActive()
+        event.target.classList.add('modal-title--active')
+    })
 })
