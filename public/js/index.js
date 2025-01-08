2a91f9effbf210f2a6d65c6d9504d953e09ad804
hojjat sadafi <161528589+hojjatSadafi@users.noreply.github.com>
2025-01-08 17:47:24 +0330

    complete responsives

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,8 @@
 const headerElem = document.querySelector('.header')
+const mobileNav = document.querySelector('.mobile-nav')
+const mobileNavCloseBtn = document.querySelector('.mobile-nav__close-btn')
+const hamburger = document.querySelector('.hamburger')
+const navOverlay = document.querySelector('.nav-overlay')
 
 window.addEventListener('scroll',event=>{
     if(document.documentElement.scrollTop){
@@ -6,5 +10,20 @@ window.addEventListener('scroll',event=>{
     } else {
         headerElem.classList.remove('header--scroll')
     }
-    
 })
+
+// funcs
+function openMobileNav(){
+    mobileNav.classList.add('mobile-nav--active')
+    navOverlay.classList.remove('hide')
+}
+function closeMobileNav(){
+    mobileNav.classList.remove('mobile-nav--active')
+    navOverlay.classList.add('hide')
+}
+
+// event
+
+hamburger.addEventListener('click',openMobileNav)
+navOverlay.addEventListener('click',closeMobileNav)
+mobileNavCloseBtn.addEventListener('click',closeMobileNav)
